5279650c79d679e78de142526cf6ff06f91f834f
brenobhnl <55766755+brenobhnl@users.noreply.github.com>
2020-07-23 16:20:14 -0300

    up app-react

---
+++ src/components/minha-primeira-app/src/components/questao01.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+function mostrarNome(props) {
+  name = "Breno Henrique";
+  return <h1>Meu nome é:, {props.name}</h1>;
+  }
+
+  function mostrarCurso(props) {
+    curso = "Sistema de Informação";
+    return <h1>Meu curso é:, {props.curso}</h1>;
+    }
+
+    function mostrarCidade(props) {
+        cidade = "Morada Nova - CE";
+        return <h1>Minha cidade de origem é:, {props.cidade}</h1>;
+        }
+        class App extends Component {
+            render();
+}
